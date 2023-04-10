21f113beacb6b7a958a366c5d8da46ec6dd6fbb7
sandeeps1229 <singhsandeep.1229@gmail.com>
2023-04-10 13:31:48 +1200

    add code

---
+++ CivicTestApril/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CivicTestApril.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+        public string? FullName { get; set; }
+        public string? ContactNumber { get; set; }
+        public string? Address { get; set; }
+        public string? OrderType { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ CivicTestApril/Program.cs
@@ -0,0 +1,40 @@
+using CivicTestApril.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using CivicTestApril;
+using CivicTestApril.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<WeatherForecastService>();
+/* Database Context Dependency Injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
